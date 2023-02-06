87b2a0813873289de0b8e3e3f4bd96f8ee10d435
LAPTOP-2V5E6HUA\Jessica Yeager <jessicayeager>
2023-02-06 14:10:40 -0500

    created a new class with new examples

--- ClassExamples/src/com/tek/example1/FirstClass.java
+++ ClassExamples/src/com/tek/example1/FirstClass.java
@@ -1,10 +1,28 @@
 package com.tek.example1;
 
 public class FirstClass {
+	//this is a comment to practice one line comments
+	
+	/*
+	 * this is going to indicate a longer comment,
+	 * maybe one that spans over multiple lines,
+	 * like so.
+	 */
 	
 	public static void main( String[] args ) {
 		System.out.println("First Java Program");
 		
+		//successfully prints quotations around the string, aka escaping 
+		System.out.println("\"Lets say I want to print this with quotations\"");
+		
+		
+		
+		
+		//this is to practice a for loop
+		for(int i = 1; i <5; i++) {
+			//contents of a for loop
+		}
+
 		int i = 5 + 5;
 		System.out.println("5 + 5 = " + i);
 	}
