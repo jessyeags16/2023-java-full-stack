ef931f294d97826bd98fab594820acbc4234a429
LAPTOP-2V5E6HUA\Jessica Yeager <jessicayeager>
2023-02-13 21:46:23 -0500

    Monster Assignment

---
+++ ClassExamples/src/monsterAssignment/Monster.java
@@ -0,0 +1,18 @@
+package monsterAssignment;
+
+public class Monster {
+	private String name;
+	
+	public Monster(String name) {
+		this.name = name;
+	}
+		public String attack() {
+			return "!^_&^$@+%$* I don't know how to attack!";
+		}
+	
+	public Monster() {
+		
+	}
+		
+
+}
---
+++ ClassExamples/src/monsterAssignment/WaterMonster.java
@@ -0,0 +1,14 @@
+package monsterAssignment;
+
+public class WaterMonster extends Monster {
+	
+	public WaterMonster(String name) {
+		super(name);
+	}
+	
+	@Override
+	public String attack() {
+		return "Attack with water!";
+	}
+
+}
